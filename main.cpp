5b59687311ced70757d60f6611aa02542a156357
Ansh Gandhi <ansh.goodboy@gmail.com>
2024-04-27 21:14:44 +0000

    robit works!!

--- main.cpp
+++ main.cpp
@@ -7,10 +7,13 @@
 #define COMMAND_FILTER 1
 #define PI 3.14159265359
 #define RAD_TO_DEG 180/PI
-#define TILT_ANGLE PI/30
 
-float kP = -4;
-float kD = -0.3;
+#define TILT_ANGLE 0.05
+#define ROTATION_SPEED 0.3
+#define RESPONSE_CYCLES 6
+
+float kP = -10.5;
+float kD = 1.0;
 float kF = 0;
 
 Serial bluetooth(p13, p14);
@@ -27,7 +30,11 @@ float gyroAngle = -PI/2;
 float currentAngle = 0;
 float lastAngle = 0;
 
-float targetAngle = -0.01;
+int forwardTarget = 0;
+int forwardCommand = 0;
+float rotationCommand = 0;
+
+float targetAngle = 0;
 float lastCommand = 0;
 bool enable = false;
 int adjust = 0;
@@ -55,7 +62,7 @@ void measure() {
         accelAngle += 2*PI;
     }
 
-    float gyroVel = (imu.gy - gyroBias) * -0.01 * 0.54 / 1000;
+    float gyroVel = (gyroBias - imu.gy) * 0.00001 * 0.54;
     gyroAngle += gyroVel;
 
     currentAngle = GYRO_CONFIDENCE * (currentAngle + gyroVel) + (1 - GYRO_CONFIDENCE) * accelAngle;
@@ -67,7 +74,7 @@ void measure() {
 }
 
 void controller() {
-    float p = currentAngle - targetAngle;
+    float p = currentAngle - targetAngle - forwardCommand * TILT_ANGLE / RESPONSE_CYCLES;
     float d = currentAngle - lastAngle;
     float f = tan(currentAngle);
     lastAngle = currentAngle;
@@ -79,12 +86,12 @@ void controller() {
 
     c *= enable;
     lastCommand = c;
-    leftMotor.speed(c);
-    rightMotor.speed(-c);
+    leftMotor.speed(c + rotationCommand);
+    rightMotor.speed(-c + rotationCommand);
 
-    serial.printf("%d, %+1.2f, %+1.3f, %+1.2f, %+1.2f, %+3.2f, %+3.2f, %+3.2f, %+3.2f \n", adjust, kP, kD, kF, c, currentAngle * RAD_TO_DEG, targetAngle * RAD_TO_DEG, gyroAngle * RAD_TO_DEG, accelAngle * RAD_TO_DEG);
+    serial.printf("%d, %+1.2f, %+1.3f, %+1.2f, %+1.2f, %+3.2f, %+3.2f, %+3.2f, %+3.2f, %d \n", adjust, kP, kD, kF, c, currentAngle * RAD_TO_DEG, targetAngle * RAD_TO_DEG, gyroAngle * RAD_TO_DEG, accelAngle * RAD_TO_DEG, forwardCommand);
     if (bluetooth.writeable()) {
-        bluetooth.printf("%d, %+1.2f, %+1.3f, %+1.2f, %+1.2f, %+3.2f, %+3.2f, %+3.2f, %+3.2f \n", adjust, kP, kD, kF, c, currentAngle * RAD_TO_DEG, targetAngle * RAD_TO_DEG, gyroAngle * RAD_TO_DEG, accelAngle * RAD_TO_DEG);
+        bluetooth.printf("%d, %+1.2f, %+1.3f, %+1.2f, %+1.2f, %+3.2f, %+3.2f, %+3.2f, %+3.2f, %d \n", adjust, kP, kD, kF, c, currentAngle * RAD_TO_DEG, targetAngle * RAD_TO_DEG, gyroAngle * RAD_TO_DEG, accelAngle * RAD_TO_DEG, forwardCommand);
     }
 }
 
@@ -93,7 +100,6 @@ int main() {
     calibrateIMU();
 
     while (1) {
-
         if (!bluetooth.readable()) {
             measure();
             controller();
@@ -109,7 +115,7 @@ int main() {
                             break;
                         case '2':
                             if (bluetooth.getc() == '1') {
-                                adjust = (adjust + 1) % 3;
+                                adjust = (adjust + 1) % 4;
                             }
                             break;
                         case '3':
@@ -124,38 +130,46 @@ int main() {
                             break;
                         case '5':
                             if (bluetooth.getc() == '1') {
-                                targetAngle += TILT_ANGLE;
+                                forwardTarget = RESPONSE_CYCLES;
                             } else {
-                                targetAngle -= TILT_ANGLE;
+                                forwardTarget = 0;
                             }
                             break;
                         case '6':
                             if (bluetooth.getc() == '1') {
-                                targetAngle -= TILT_ANGLE;
+                                forwardTarget = -RESPONSE_CYCLES;
                             } else {
-                                targetAngle += TILT_ANGLE;
+                                forwardTarget = 0;
                             }
                             break;
                         case '7':
                             if (bluetooth.getc() == '1') {
                                 if (adjust == 0) {
-                                    kP += 0.1;
+                                    rotationCommand = ROTATION_SPEED;
                                 } else if (adjust == 1) {
-                                    kD += 0.01;
+                                    kP += 0.1;
+                                } else if (adjust == 2) {
+                                    kD += 0.1;
                                 } else {
-                                    kF += 0.01;
+                                    kF += 0.1;
                                 }
+                            } else if (adjust == 0) {
+                                rotationCommand = 0;
                             }
                             break;
                         case '8':
                             if (bluetooth.getc() == '1') {
                                 if (adjust == 0) {
-                                    kP -= 0.1;
+                                    rotationCommand = -ROTATION_SPEED;
                                 } else if (adjust == 1) {
-                                    kD -= 0.01;
+                                    kP -= 0.1;
+                                } else if (adjust == 2) {
+                                    kD -= 0.1;
                                 } else {
-                                    kF -= 0.01;
+                                    kF -= 0.1;
                                 }
+                            } else if (adjust == 0) {
+                                rotationCommand = 0;
                             }
                             break;
                         default:
@@ -164,71 +178,11 @@ int main() {
                 }
             }
         }
+
+        if (forwardTarget > forwardCommand) {
+            forwardCommand += forwardCommand >= RESPONSE_CYCLES ? 0 : 1;
+        } else if (forwardTarget < forwardCommand) {
+            forwardCommand -= forwardCommand <= -RESPONSE_CYCLES ? 0 : 1;
+        }
     }
 }
-
-// #include "LSM9DS1.h"
-// #include "mbed.h"
-
-// #define CALIBRATION_READS 200
-// #define GYRO_CONFIDENCE 0
-// #define PI 3.14159265359
-
-// // Serial bluetooth(p28, p27);
-// // LSM9DS1 imu(p9, p10, 0xD6, 0x3C);
-// Serial bluetooth(p13, p14);
-// LSM9DS1 imu(p28, p27, 0xD6, 0x3C);
-// Serial serial(USBTX, USBRX);
-
-// float accelBias = 0;
-// float gyroBias = 0;
-// float accelAngle = 0;
-// float gyroVel = 0;
-// float gyroAngle = 0;
-// float currentAngle = 0;
-
-// void calibrateIMU() {
-//     for (int i = 0; i < CALIBRATION_READS; ++i) {
-//         imu.readAccel();
-//         imu.readGyro();
-//         // accelBias += atan2((float) imu.az, (float) -imu.ay); // https://vanhunteradams.com/Pico/ReactionWheel/Complementary_Filters.html
-//         // gyroBias += imu.gx;
-//         accelBias += atan2((float) imu.az, (float) imu.ax); // https://vanhunteradams.com/Pico/ReactionWheel/Complementary_Filters.html
-//         gyroBias += imu.gy;
-//     }
-
-//     accelBias /= CALIBRATION_READS;
-//     gyroBias /= CALIBRATION_READS;
-    
-//     serial.printf("accelBias: %f\n", accelBias);
-//     serial.printf("gyroBias: %f\n", gyroBias);
-// }
-
-// void measure() {
-//     imu.readAccel();
-//     imu.readGyro();
-//     accelAngle = atan2((float) imu.az, (float) imu.ax) - accelBias;
-//     if (accelAngle < -PI) {
-//         accelAngle += 2*PI;
-//     }
-//     gyroVel = (imu.gy - gyroBias) * -0.01 * 0.5 / 1000;
-//     gyroAngle += gyroVel;
-// }
-
-// int main() {
-//     imu.begin();
-//     calibrateIMU();
-
-//     serial.printf("test\n");
-//     while (1) {
-//         measure();
-//         currentAngle = 0.98 * (currentAngle + gyroVel) + 0.02 * accelAngle;
-//         serial.printf("%+5.2f %+5.2f %+5.2f\n", gyroAngle * 180/PI, accelAngle * 180/PI, currentAngle * 180/PI);
-//         if (bluetooth.writeable()) {
-//             bluetooth.printf("%5.2f, %5.2f, %5.2f\n", gyroAngle * 180/PI, accelAngle * 180/PI, currentAngle * 180/PI);
-//         }
-//         // wait(0.1);
-//     }
-
-//     return 0;
-// }
